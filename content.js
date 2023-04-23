452bc374fd015f22bc9d795a826a11b28ae2375b
1983shiv <1983.shiv@gmail.com>
2023-04-23 15:45:24 +0530

    revised the content.js to comment out the console.log

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ function clickButton() {
     const button = document.querySelector("button.css-1x8cf1d.edgvbvh10");
     if (button) {
       button.click();
-      console.log("clicking button every second");
+    //   console.log("clicking button every second");
     }
   }
   
